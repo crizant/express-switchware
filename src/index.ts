c3bbff0ea6ecd7f259e74bfa0102c42111078657
Crizant Lai <crizant@CrizantdeMacBook-Pro.local>
2025-09-02 00:22:16 +1000

    fix: ts config

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
 
-const switchware =
-  <T extends string | number | symbol, R extends Record<T, RequestHandler>>(
-    getKey: (req: Request, res: Response) => T,
-    mapping: R
-  ) =>
-  (req: Request, res: Response, next: NextFunction) => {
+const expressSwitchware = <
+  T extends string | number | symbol,
+  R extends Record<T, RequestHandler>
+>(
+  getKey: (req: Request, res: Response) => T,
+  mapping: R
+): RequestHandler => {
+  return (req: Request, res: Response, next: NextFunction) => {
     const key = getKey(req, res);
-    if (mapping[key]) {
-      return mapping[key](req, res, next);
-    } else {
-      return next(new Error("Invalid middleware key"));
+    const handler = mapping[key];
+    if (handler) {
+      return handler(req, res, next);
     }
+    return next(new Error(`Invalid middleware key: ${String(key)}`));
   };
+};
 
-export default switchware;
+export default expressSwitchware;
